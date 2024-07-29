1f20fb026669737968335f7583d3917b80a1ca8a
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2024-07-29 16:40:07 +0200

    Initial version of ocm-gear
    This is the initial contribution to the Open Source Open-Component-Model
    OCM-Gear project.
    
    The following SAP developers contributed to this project until this
    initial contribution was published as open source:
    
       142  Jonas Brand
       115  Philipp Heil
         7  Julian Herzog
         2  Christian Cwienk
         1  Andreas Burger
    
    It is derived from the historical, SAP-internal delivery-gear repository
    at commit dd72a39f65864f48a4829775d926b2a0be63a974

---
+++ installation/generate_helm_values.py
@@ -0,0 +1,435 @@
+#!/usr/bin/env python3
+
+import argparse
+import base64
+import collections.abc
+import logging
+import os
+
+import yaml
+
+import ctx
+import model
+
+logger = logging.getLogger(__name__)
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+repo_dir = os.path.abspath(os.path.join(own_dir, os.pardir))
+
+
+def normalize_name(
+    name: str,
+) -> str:
+    return name.replace('_', '-')
+
+
+def delivery_db_cfg_if_specified(
+    cfg_set: model.ConfigurationSet,
+):
+    try:
+        return cfg_set.delivery_db()
+    except:
+        logger.warning('delivery-db cfg not found')
+
+    return None
+
+
+def delivery_db_backup_cfg_if_specified(
+    extension_cfg,
+):
+    try:
+        return extension_cfg.deliveryDbBackup()
+    except:
+        logger.warning('delivery-db-backup cfg not found')
+
+    return None
+
+
+def extension_cfgs_if_specified(
+    cfg_set: model.ConfigurationSet,
+) -> tuple:
+    try:
+        return tuple(cfg_set._cfg_elements(cfg_type_name='delivery_gear_extensions'))
+    except:
+        logger.warning('extensions cfgs not found')
+
+    return tuple()
+
+
+def enabled_extensions(
+    extension_cfgs: tuple,
+) -> set[str]:
+    enabled_extensions = set()
+
+    for extension_cfg in extension_cfgs:
+        if extension_cfg.raw.get('artefactEnumerator'):
+            enabled_extensions.add('artefactEnumerator')
+        if extension_cfg.raw.get('bdba'):
+            enabled_extensions.add('backlogController')
+            enabled_extensions.add('bdba')
+        if extension_cfg.raw.get('clamav'):
+            enabled_extensions.add('backlogController')
+            enabled_extensions.add('clamav')
+        if extension_cfg.raw.get('deliveryDbBackup'):
+            enabled_extensions.add('deliveryDbBackup')
+        if extension_cfg.raw.get('issueReplicator'):
+            enabled_extensions.add('backlogController')
+            enabled_extensions.add('issueReplicator')
+
+    return enabled_extensions
+
+
+def pod_helm_values(
+    cfg,
+) -> dict:
+    annotations = dict()
+    labels = dict()
+
+    try:
+        annotations = cfg.pod()['annotations']
+        labels = cfg.pod()['labels']
+    except AttributeError:
+        pass
+
+    return {
+        'annotations': annotations,
+        'labels': labels,
+    }
+
+
+def ingress_helm_values(
+    cfg,
+    cfg_set: model.ConfigurationSet,
+) -> dict:
+    return {
+        'annotations': cfg.ingress().get('annotations', dict()),
+        'class': cfg.ingress().get('class', 'nginx'),
+        'hosts': cfg.ingress().get('hosts', []),
+        'tlsHostNames': cfg_set.ingress().tls_host_names(),
+    }
+
+
+def serialise_cfg_factory(
+    cfg_factory: model.ConfigFactory,
+    cfg_set: model.ConfigurationSet,
+) -> str:
+    # serialise cfg factory as json
+    cfg_sets = [cfg_set.cfg_set()]
+    serialiser = model.ConfigSetSerialiser(
+        cfg_sets=cfg_sets,
+        cfg_factory=cfg_factory,
+    )
+    serialised = serialiser.serialise().encode('utf-8')
+    return base64.b64encode(serialised).decode('utf-8')
+
+
+def common_env_vars(
+    cfg_set: model.ConfigurationSet,
+    namespace: str,
+) -> dict[str, str]:
+    env_vars = {
+        'CFG_FACTORY_SECRET_PATH': '/cfg_factory/cfg_factory',
+        'FEATURES_CFG_PATH': '/features_cfg/features_cfg',
+        'K8S_TARGET_NAMESPACE': namespace,
+    }
+
+    try:
+        kubernetes_cfg_name = cfg_set.cfg_set().kubernetes().name()
+        env_vars['K8S_CFG_NAME'] = kubernetes_cfg_name
+    except:
+        pass
+
+    return env_vars
+
+
+def delivery_db_helm_values(
+    cfg_set: model.ConfigurationSet,
+):
+    if not (delivery_db_cfg := delivery_db_cfg_if_specified(
+        cfg_set=cfg_set,
+    )):
+        return dict()
+
+    helm_values = delivery_db_cfg.helm_values()
+    helm_values['postgresqlPassword'] = delivery_db_cfg.credentials().password()
+
+    return helm_values
+
+
+def delivery_service_helm_values(
+    cfg_set: model.ConfigurationSet,
+    namespace: str,
+    create_cfg_factory: bool=False,
+    cfg_factory: model.ConfigFactory=None,
+) -> dict:
+    delivery_service_cfg = cfg_set.delivery_service()
+
+    env_vars = common_env_vars(
+        cfg_set=cfg_set,
+        namespace=namespace,
+    )
+
+    try:
+        extra_env_vars = [
+            [
+                env_var.get('name'),
+                f'"{env_var.get("value")}"',
+            ]
+            for env_var in delivery_service_cfg.env()
+        ]
+        env_vars = env_vars | dict(extra_env_vars)
+    except AttributeError:
+        pass
+
+    helm_values = {
+        'args': [
+            '--delivery-cfg',
+            cfg_set.delivery().name(),
+            '--delivery-endpoints',
+            cfg_set.delivery_endpoints().name(),
+        ],
+        'envVars': env_vars,
+        'pod': pod_helm_values(
+            cfg=delivery_service_cfg,
+        ),
+        'ingress': ingress_helm_values(
+            cfg=delivery_service_cfg,
+            cfg_set=cfg_set,
+        ),
+        'replicas': delivery_service_cfg.replicas(),
+        'featuresCfg': delivery_service_cfg.features_cfg(),
+        'createCfgFactorySecret': create_cfg_factory,
+    }
+
+    if delivery_db_cfg := delivery_db_cfg_if_specified(
+        cfg_set=cfg_set,
+    ):
+        helm_values['args'].append('--delivery-db-cfg')
+        helm_values['args'].append(delivery_db_cfg.name())
+
+    if delivery_service_cfg.invalid_semver_ok():
+        helm_values['args'].append('--invalid-semver-ok')
+
+    extension_cfgs = extension_cfgs_if_specified(
+        cfg_set=cfg_set,
+    )
+    if extensions := enabled_extensions(
+        extension_cfgs=extension_cfgs,
+    ):
+        helm_values['args'].append('--service-extensions')
+        helm_values['args'].extend(extensions)
+
+    if create_cfg_factory:
+        helm_values['cfgFactory'] = serialise_cfg_factory(
+            cfg_factory=cfg_factory,
+            cfg_set=cfg_set,
+        )
+
+    return helm_values
+
+
+def delivery_dashboard_helm_values(
+    cfg_set: model.ConfigurationSet,
+) -> dict:
+    delivery_service_cfg = cfg_set.delivery_service()
+    delivery_dashboard_cfg = cfg_set.delivery_dashboard()
+
+    delivery_service_url = (
+        f'{delivery_service_cfg.protocol()}://'
+        f'{delivery_service_cfg.ingress()["hosts"][0]}'
+    )
+
+    return {
+        'envVars': {
+            'REACT_APP_DELIVERY_SERVICE_API_URL': delivery_service_url,
+        },
+        'pod': pod_helm_values(
+            cfg=delivery_dashboard_cfg,
+        ),
+        'ingress': ingress_helm_values(
+            cfg=delivery_dashboard_cfg,
+            cfg_set=cfg_set,
+        ),
+        'replicas': delivery_dashboard_cfg.replicas(),
+    }
+
+
+def extensions_helm_values(
+    cfg_set: model.ConfigurationSet,
+    namespace: str,
+):
+    env_vars = common_env_vars(
+        cfg_set=cfg_set,
+        namespace=namespace,
+    )
+
+    extension_cfgs = extension_cfgs_if_specified(
+        cfg_set=cfg_set,
+    )
+
+    def iter_delivery_db_backup_cfg(extension_cfg):
+        db_backup_cfg = delivery_db_backup_cfg_if_specified(extension_cfg)
+
+        if not db_backup_cfg:
+            return
+
+        yield 'DELIVERY_GEAR_CFG_NAME', normalize_name(extension_cfg.name())
+        yield 'SCHEDULE', db_backup_cfg['schedule']
+        yield 'SUCCESSFUL_JOBS_HISTORY_LIMIT', db_backup_cfg['successful_jobs_history_limit']
+        yield 'FAILED_JOBS_HISTORY_LIMIT', db_backup_cfg['failed_jobs_history_limit']
+
+    delivery_db_backup_cfgs = [
+        dict(
+            iter_delivery_db_backup_cfg(
+                extension_cfg=extension_cfg,
+            ),
+            **env_vars,
+        )
+        for extension_cfg in extension_cfgs
+    ]
+
+    def iter_helm_values() -> collections.abc.Generator[tuple[str, dict], None, None]:
+        extensions = enabled_extensions(
+            extension_cfgs=extension_cfgs,
+        )
+
+        artefact_enumerator_enabled = 'artefactEnumerator' in extensions
+        backlog_controller_enabled = 'backlogController' in extensions
+        delivery_db_backup_enabled = 'deliveryDbBackup' in extensions
+        freshclam_enabled = 'clamav' in extensions
+
+        configuration = {
+            'scanConfigurations': [
+                {
+                    'name': normalize_name(extension_cfg.name()),
+                    'spec': extension_cfg.raw,
+                } for extension_cfg in extension_cfgs
+            ],
+        }
+        yield 'configuration', configuration
+
+        if artefact_enumerator_enabled:
+            artefact_enumerator = {
+                'enabled': True,
+                'envVars': env_vars,
+                'configuration': {},
+            }
+            yield 'artefact-enumerator', artefact_enumerator
+
+        if backlog_controller_enabled:
+            backlog_controller = {
+                'enabled': True,
+                'envVars': env_vars,
+                'scanConfigurations': [
+                    normalize_name(extension_cfg.name())
+                    for extension_cfg in extension_cfgs
+                ],
+                'namespace': namespace,
+            }
+            yield 'backlog-controller', backlog_controller
+
+        if delivery_db_backup_enabled:
+            delivery_db_backup = {
+                'enabled': True,
+                'configurations': delivery_db_backup_cfgs,
+            }
+            yield 'delivery-db-backup', delivery_db_backup
+
+        if freshclam_enabled:
+            freshclam = {
+                'enabled': True,
+            }
+            yield 'freshclam', freshclam
+
+    return dict(iter_helm_values())
+
+
+def write_values_to_file(
+    helm_values: dict,
+    out_file: str,
+):
+    with open(out_file, 'w') as file:
+        file.write(yaml.safe_dump(helm_values))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--cfg-dir',
+        required=False,
+    )
+    parser.add_argument(
+        '--cfg-set',
+        required=True,
+    )
+    parser.add_argument(
+        '--create-cfg-factory',
+        action='store_true',
+        default=False,
+    )
+    parser.add_argument(
+        '--namespace',
+        default='delivery',
+    )
+    parser.add_argument(
+        '--out-dir',
+        default=os.path.join(own_dir, 'helm-values'),
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    parsed_arguments = parse_args()
+
+    cfg_dir = parsed_arguments.cfg_dir
+    cfg_set_name = parsed_arguments.cfg_set
+    create_cfg_factory = parsed_arguments.create_cfg_factory
+    namespace = parsed_arguments.namespace
+    out_dir = parsed_arguments.out_dir
+
+    if cfg_dir:
+        cfg_factory = model.ConfigFactory.from_cfg_dir(cfg_dir)
+    else:
+        cfg_factory = ctx.cfg_factory()
+
+    cfg_set = cfg_factory.cfg_set(cfg_set_name)
+
+    os.makedirs(
+        name=out_dir,
+        exist_ok=True,
+    )
+
+    write_values_to_file(
+        helm_values=delivery_db_helm_values(
+            cfg_set=cfg_set,
+        ),
+        out_file=os.path.join(out_dir, 'values-delivery-db.yaml'),
+    )
+    write_values_to_file(
+        helm_values=delivery_service_helm_values(
+            cfg_set=cfg_set,
+            namespace=namespace,
+            create_cfg_factory=create_cfg_factory,
+            cfg_factory=cfg_factory,
+        ),
+        out_file=os.path.join(out_dir, 'values-delivery-service.yaml'),
+    )
+    write_values_to_file(
+        helm_values=delivery_dashboard_helm_values(
+            cfg_set=cfg_set,
+        ),
+        out_file=os.path.join(out_dir, 'values-delivery-dashboard.yaml'),
+    )
+    write_values_to_file(
+        helm_values=extensions_helm_values(
+            cfg_set=cfg_set,
+            namespace=namespace,
+        ),
+        out_file=os.path.join(out_dir, 'values-extensions.yaml'),
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ installation/install.sh
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+OWN_DIR=$(readlink -f "$(dirname "${0}")")
+
+CFG_DIR=""
+CFG_SET=""
+CREATE_CFG_FACTORY=""
+INGRESS_NAMESPACE=ingress-nginx
+INSTALL_INGRESS_CONTROLLER=""
+KUBECONFIG=""
+KUBERNETES_CFG_NAME=""
+NAMESPACE=delivery
+OCM_GEAR_VERSION=""
+POSTGRES_VERSION=10.12.4
+VALUES_DIR="${OWN_DIR}/helm-values"
+
+parse_flags() {
+  while test $# -gt 0; do
+    case "$1" in
+    --cfg-dir)
+      shift; CFG_DIR="$1"
+      ;;
+    --cfg-set)
+      shift; CFG_SET="$1"
+      ;;
+    --create-cfg-factory)
+      CREATE_CFG_FACTORY=true
+      ;;
+    --ingress-namespace)
+      shift; INGRESS_NAMESPACE="$1"
+      ;;
+    --install-ingress-controller)
+      INSTALL_INGRESS_CONTROLLER=true
+      ;;
+    --kubeconfig)
+      shift; KUBECONFIG="$1"
+      ;;
+    --kubernetes-cfg-name)
+      shift; KUBERNETES_CFG_NAME="$1"
+      ;;
+    --namespace)
+      shift; NAMESPACE="$1"
+      ;;
+    --postgres-version)
+      shift; POSTGRES_VERSION="$1"
+      ;;
+    --values-dir)
+      shift; VALUES_DIR="$1"
+      ;;
+    --version)
+      shift; OCM_GEAR_VERSION="$1"
+      ;;
+    esac
+
+    shift
+  done
+}
+
+parse_kubeconfig() {
+  if [ -n "${KUBERNETES_CFG_NAME}" ]; then
+    cli.py config export_kubeconfig --kubernetes-config-name ${KUBERNETES_CFG_NAME} --output-file "${OWN_DIR}/kubeconfig.yaml"
+    export KUBECONFIG="${OWN_DIR}/kubeconfig.yaml"
+  fi
+}
+
+check_required_flags() {
+  flags=("$@")
+  flag_unset=""
+
+  for flag in "${flags[@]}"; do
+    [ -z "${!flag}" ] && echo "--$(echo ${flag} | tr '[:upper:]' '[:lower:]' | tr '_' '-') must be set" && flag_unset=true
+  done
+
+  [ -n "${flag_unset}" ] && exit 1
+
+  return 0
+}
+
+parse_flags "$@"
+parse_kubeconfig
+check_required_flags INGRESS_NAMESPACE KUBECONFIG NAMESPACE POSTGRES_VERSION VALUES_DIR
+
+if ! which ocm 1>/dev/null; then
+  echo ">>> Installing OCM cli..."
+  curl -s https://ocm.software/install.sh | bash
+  echo ">>> Installed OCM cli in version $(ocm version)"
+fi
+if ! which yq 1>/dev/null; then
+  echo ">>> Installing yq..."
+  VERSION=v4.44.2 && BINARY=yq_linux_amd64 && wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && mv ${BINARY} /usr/bin/yq
+  echo ">>> Installed yq in version $(yq --version)"
+fi
+if ! which helm 1>/dev/null; then
+  echo ">>> Installing Helm package manager..."
+  DESIRED_VERSION=v3.7.0 && curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+  echo ">>> Installed Helm packaged manager in version $(helm version)"
+fi
+if ! which kubectl 1>/dev/null; then
+  echo ">>> Installing kubectl..."
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && mv kubectl /usr/bin/kubectl
+  echo ">>> Installed kubectl in version $(kubectl version)"
+fi
+
+OCM_GEAR_COMPONENT_REF="europe-docker.pkg.dev/gardener-project/releases//ocm.software/ocm-gear"
+OCM_GEAR_VERSION="${OCM_GEAR_VERSION:-$(ocm show versions ${OCM_GEAR_COMPONENT_REF} | tail -1)}"
+COMPONENT_DESCRIPTORS=$(ocm get cv ${OCM_GEAR_COMPONENT_REF}:${OCM_GEAR_VERSION} -o yaml -r)
+
+echo ">>> Installing OCM-Gear in version ${OCM_GEAR_VERSION}"
+
+DELIVERY_SERVICE_CHART=$(echo "${COMPONENT_DESCRIPTORS}" | yq eval '.component.resources.[] | select(.name == "delivery-service" and .type == "helmChart/v1") | .access.imageReference')
+DELIVERY_DASHBOARD_CHART=$(echo "${COMPONENT_DESCRIPTORS}" | yq eval '.component.resources.[] | select(.name == "delivery-dashboard" and .type == "helmChart/v1") | .access.imageReference')
+EXTENSIONS_CHART=$(echo "${COMPONENT_DESCRIPTORS}" | yq eval '.component.resources.[] | select(.name == "extensions" and .type == "helmChart/v1") | .access.imageReference')
+DELIVERY_DB_CHART="europe-docker.pkg.dev/gardener-project/releases/delivery-gear/delivery-charts/postgresql"
+
+if [ ! -d "${VALUES_DIR}" ]; then
+  echo ">>> Generating required helm values into ${VALUES_DIR}"
+  python3 ${OWN_DIR}/generate_helm_values.py \
+    ${CFG_DIR:+"--cfg-dir"} ${CFG_DIR:+${CFG_DIR}} \
+    ${CFG_SET:+"--cfg-set"} ${CFG_SET:+${CFG_SET}} \
+    ${CREATE_CFG_FACTORY:+"--create-cfg-factory"} \
+    --namespace ${NAMESPACE} \
+    --out-dir ${VALUES_DIR}
+else
+  echo ">>> Found existing helm values directory ${VALUES_DIR}, will not generate helm values"
+fi
+
+if [ -n "${INSTALL_INGRESS_CONTROLLER}" ]; then
+  echo ">>> Creating namespace ${INGRESS_NAMESPACE}"
+  kubectl create ns ${INGRESS_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+  kubectl config set-context --current --namespace=${INGRESS_NAMESPACE}
+
+  echo ">>> Installing ingress nginx controller"
+  helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+  helm repo update
+  helm upgrade -i ingress-nginx ingress-nginx/ingress-nginx \
+    --namespace ${INGRESS_NAMESPACE} \
+    --set externalTrafficPolicy=Cluster
+  echo ">>> Waiting for ingress nginx controller to become ready, this can take up to 90 seconds..."
+  kubectl wait \
+    --namespace ${INGRESS_NAMESPACE} \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=90s
+fi
+
+echo ">>> Installing OCM-Gear components"
+
+echo ">>> Creating namespace ${NAMESPACE}"
+kubectl create ns ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+kubectl config set-context --current --namespace=$NAMESPACE
+
+echo ">>> Installing delivery-db from ${DELIVERY_DB_CHART}"
+helm upgrade -i delivery-db oci://${DELIVERY_DB_CHART} \
+  --namespace ${NAMESPACE} \
+  --version ${POSTGRES_VERSION} \
+  --values ${VALUES_DIR}/values-delivery-db.yaml
+
+echo ">>> Installing delivery-service from ${DELIVERY_SERVICE_CHART}"
+helm upgrade -i delivery-service oci://${DELIVERY_SERVICE_CHART%:*} \
+  --namespace ${NAMESPACE} \
+  --version ${DELIVERY_SERVICE_CHART#*:} \
+  --values ${VALUES_DIR}/values-delivery-service.yaml
+echo ">>> Waiting for delivery-service to become ready, this can take up to 3 minutes..."
+kubectl rollout status deployment delivery-service \
+  --namespace ${NAMESPACE} \
+  --timeout=180s
+
+echo ">>> Installing delivery-dashboard from ${DELIVERY_DASHBOARD_CHART}"
+helm upgrade -i delivery-dashboard oci://${DELIVERY_DASHBOARD_CHART%:*} \
+  --namespace ${NAMESPACE} \
+  --version ${DELIVERY_DASHBOARD_CHART#*:} \
+  --values ${VALUES_DIR}/values-delivery-dashboard.yaml
+
+echo ">>> Installing extensions from ${EXTENSIONS_CHART}"
+helm upgrade -i extensions oci://${EXTENSIONS_CHART%:*} \
+  --namespace ${NAMESPACE} \
+  --version ${EXTENSIONS_CHART#*:} \
+  --values ${VALUES_DIR}/values-extensions.yaml
